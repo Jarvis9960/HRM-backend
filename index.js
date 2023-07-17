19e2ba1d81f4383784b2e85d84aa25bbf27dcf0f
durgesh-ajile <durgesh.rajak@ajiledone.com>
2023-07-17 15:09:17 +0530

    adding folders

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const app = express();
+const PORT = 5000
+
+app.get('/', (req, res)=>{
+    res.send('Hello world')
+})
+
+
+app.listen(PORT, () => {
+    console.log(`server is running on PORT ${PORT}`)
+})
