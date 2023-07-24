248f67e3deb4d38fd7f4da77c395492a9739d3c0
Jarvis9960 <ankitfukte11@gmail.com>
2023-07-24 13:36:17 +0530

    fixing authetication middleware and configure multer middleware to take images only

--- Middlewares/tokenCheckContractor.js
+++ Middlewares/tokenCheckContractor.js
@@ -2,6 +2,7 @@ import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 import path from "path";
 dotenv.config({ path: path.resolve("../config.env") });
+import ContractorModel from "../Models/ContractorModel.js";
 
 export const tokenCheckContractor = async (req, res, next) => {
   let token;
@@ -18,8 +19,9 @@ export const tokenCheckContractor = async (req, res, next) => {
 
       const decoded = jwt.verify(token, process.env.JWT_SECRET2);
 
+      const checkContractor = await ContractorModel.findOne({_id: decoded._id}).select("-password");
 
-      req.user = decoded._id;
+      req.user = checkContractor;
      
       next();
     } catch (error) {
--- Routes/ContractorRoute.js
+++ Routes/ContractorRoute.js
@@ -1,20 +1,43 @@
 import express from "express";
 const router = express.Router();
-import { createContractor, loginContractor, updatecontractorprofile, getContractor, approveContractor, getdetailsofContractor } from "../Controllers/ContractorController.js";
-import { multerUpload } from "../Middlewares/MulterUpload.js";
+import {
+  createContractor,
+  loginContractor,
+  updatecontractorprofile,
+  getContractor,
+  approveContractor,
+  getdetailsofContractor,
+} from "../Controllers/ContractorController.js";
+import { storage, checkFileType } from "../Middlewares/MulterUpload.js";
 import { tokenCheckadmin } from "../Middlewares/tokenCheckadmin .js";
 import { tokenCheckContractor } from "../Middlewares/tokenCheckContractor.js";
+import multer from "multer";
 
-const multerUploadfiles = multerUpload.fields([{ name: 'actualPanImage'}, { name: 'actualAdharImage'}, { name: 'beneficiaryPanImage'}, { name: 'beneficiaryAadharImage'}])
+const multerUpload = multer({
+  storage,
+  fileFilter: function (req, file, cb) {
+    checkFileType(file, cb);
+  },
+});
+
+const multerUploadfiles = multerUpload.fields([
+  { name: "actualPanImage" },
+  { name: "actualAdharImage" },
+  { name: "beneficiaryPanImage" },
+  { name: "beneficiaryAadharImage" },
+]);
 
 router.post("/addcontractor", tokenCheckadmin, createContractor);
 router.get("/getContractor", tokenCheckadmin, getContractor);
 router.patch("/approveContractor", tokenCheckadmin, approveContractor);
 router.get("/getdetailsofContractor", tokenCheckadmin, getdetailsofContractor);
 
-router.post("/loginContractor", loginContractor)
-router.post("/updatecontractorprofile", tokenCheckContractor,  multerUploadfiles, updatecontractorprofile);
-
+router.post("/loginContractor", loginContractor);
+router.post(
+  "/updatecontractorprofile",
+  tokenCheckContractor,
+  multerUploadfiles,
+  updatecontractorprofile
+);
 
 export default router;
-
