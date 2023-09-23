f1304613491fbe68777f10bea5d0ac287060d355
Jarvis9960 <ankitfukte11@gmail.com>
2023-09-23 16:01:08 +0530

    completing work for sending mail attachments

---
+++ Models/InvoiceModel.js
@@ -0,0 +1,6 @@
+// import mongoose from "mongoose";
+
+
+// const InvoiceModelSchema = new mongoose.Schema({
+
+// })
--- Routes/SendMailRoute.js
+++ Routes/SendMailRoute.js
@@ -1,12 +1,50 @@
 import express from "express";
 const router = express.Router();
-import { getUserConsent, microsoftLoginController, sendMailFromContractorMail } from "../Controllers/SendMailController.js"
+import {
+  getUserConsent,
+  microsoftLoginController,
+  sendMailFromContractorMail,
+} from "../Controllers/SendMailController.js";
 import { tokenCheckcontractor } from "../Middlewares/tokenCheckcontractor.js";
+import multer from "multer";
+import { fileURLToPath } from "url";
+import path, { dirname } from "path";
+let fileName = fileURLToPath(import.meta.url);
+let __dirName = dirname(fileName);
 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirName, "../public/uploads/Invoices"));
+  },
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      file.originalname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
+
+function checkFileType(file, cb) {
+  const filetypes = /csv/;
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = filetypes.test(file.mimetype);
+
+  if (extname && mimetype) {
+    return cb(null, true);
+  } else {
+    cb("CSV File only!"); // custom this message to fit your needs
+  }
+}
+
+const upload = multer({
+  storage,
+  fileFilter: function (req, file, cb) {
+    checkFileType(file, cb);
+  },
+});
 
 router.get("/get-access-token-microsoft", microsoftLoginController);
 router.get("/get-user-consent", getUserConsent);
-router.post("/send-mail-of-timesheet", sendMailFromContractorMail)
-
+router.post("/send-mail-of-timesheet", upload.single("TimesheetFile"), sendMailFromContractorMail);
 
 export default router;
