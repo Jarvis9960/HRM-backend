7ae3c554485f0e59147e7563ce18a26b0a50a664
Jarvis9960 <ankitfukte11@gmail.com>
2023-09-26 15:06:34 +0530

    debugging and fixing issue

--- Routes/ClientRoute.js
+++ Routes/ClientRoute.js
@@ -5,10 +5,11 @@ const router = express.Router();
 
 router.post("/createClient", tokenCheckadmin, createClient);
 router.get("/getallClient", tokenCheckadmin, getClientForAdmin);
-router.patch("/updateOrganizationtoclient", tokenCheckadmin, updateOrganizationOfOrganization);
+router.patch("/updateOrganizationtofclient", tokenCheckadmin, updateOrganizationOfOrganization);
 router.get("/getPO's", tokenCheckadmin, getPOOfClients);
 router.get("/getsinglePo", tokenCheckadmin, getSinglePO);
 router.post("/createPo", tokenCheckadmin, createPO);
+
 
 
 export default router;
