d608d928cd2d9bf8ff58001884ea833f9b6aead4
Jarvis9960 <ankitfukte11@gmail.com>
2023-10-11 13:40:04 +0530

    made a api for getting timesheet task for admin

--- Routes/TimesheetRoute.js
+++ Routes/TimesheetRoute.js
@@ -3,13 +3,20 @@ const router = express.Router();
 
 import { tokenCheckadmin } from "../Middlewares/tokenCheckadmin .js";
 import { tokenCheckcontractor } from "../Middlewares/tokenCheckcontractor.js";
-import { createTask, getallTasks, getownTasks, updateTask } from "../Controllers/TaskController.js";
+import {
+  createTask,
+  getallTasks,
+  getownTasks,
+  getownTasksAdmin,
+  updateTask,
+} from "../Controllers/TaskController.js";
 // Create a task for a particular date
-router.post('/createTask', tokenCheckcontractor, createTask);
+router.post("/createTask", tokenCheckcontractor, createTask);
 
 // Get saved tasks for a particular date
 router.get("/getallTasks", tokenCheckadmin, getallTasks);
 router.get("/getTasks", tokenCheckcontractor, getownTasks);
+router.get("/gettaskforadmin", tokenCheckadmin, getownTasksAdmin);
 router.patch("/updateTask", tokenCheckcontractor, updateTask);
 
 export default router;
