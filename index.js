de6ba18c8535a4d5c16f92033cc9ba464846f874
Jarvis9960 <ankitfukte11@gmail.com>
2023-11-01 20:11:32 +0530

    adding frontend url to allow cors error

--- index.js
+++ index.js
@@ -37,6 +37,8 @@ const allowedOrigins = [
   "http://localhost:6000",
   "http://127.0.0.1:5173",
   "http://127.0.0.1:4000",
+  "https://driven-utility-403612.el.r.appspot.com",
+  "https://admin-portal-403714.el.r.appspot.com"
 ];
 
 app.use(
