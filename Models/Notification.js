f26375807df0fcfb6e39d6892cd7bd6eab08e869
Jarvis9960 <ankitfukte11@gmail.com>
2023-11-18 10:40:39 +0530

    adding notificationfor to identify notification

--- Models/Notification.js
+++ Models/Notification.js
@@ -22,6 +22,10 @@ const notificationSchema = new mongoose.Schema(
       type: Boolean,
       default: false,
     },
+    NotificationFor: {
+      type: String,
+      required: true,
+    },
   },
   { timestamps: true }
 );
